35323294792422ab34dacdded030199f166c8d25
Rocket Scientist <2016meb1141@iitrpr.ac.in>
2020-05-21 12:20:05 +0530

    Initial Commit

---
+++ NeedForUpwindScheme_Visualisation/README.md
@@ -0,0 +1,31 @@
+# Need for Upwind Scheme: A visualisation
+
+Need for upwind scheme in numerical solutions for advection-diffusion type problems is illustrated with the solution of 1-D steady state axial convection-diffusion slug flow problem.The below library shows the exact solution and the Finite Difference Scheme solution for different ranges of peclet number
+
+
+## Getting started 
+
+The repository can be cloned and the library `Visualise.py` can be simply executed for the visualisation
+
+### Prerequisites 
+
+The library uses the following two python libraries
+
+* numpy
+* math
+* matplotlib
+
+### Problem statement
+
+To be updated soon
+
+## Usage
+
+To be updated soon
+
+![The solution for different Peclet number](/Images/Example.png)
+
+
+
+
+
---
+++ NeedForUpwindScheme_Visualisation/Visualise.py
@@ -0,0 +1,57 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+def TDMA(A,b):														#solve Ax = b using TDM algorithm
+    p=[]
+    q=[]
+    p.append(-1*A[0][1]/A[0][0])
+    q.append(b[0]/A[0][0])
+    x=np.zeros(len(b))
+    for i in range(1,len(b)):
+        if i!= len(b)-1:
+            p.append(-1*A[i][i+1]/(A[i][i]+A[i][i-1]*p[i-1]))
+        q.append((b[i]-A[i][i-1]*q[i-1])/(A[i][i]+A[i][i-1]*p[i-1]))
+    x[len(b)-1]=q[len(b)-1]
+    for i in range(len(b)-2,-1,-1):
+        x[i]=p[i]*x[i+1]+q[i]
+    return x 
+def visulaise(peclet):
+    length=peclet.size
+    fig, axs = plt.subplots(length,figsize=(4,10))
+    plt.ylim(-1,1)
+    for iterator in range(length):
+        n   =   100                                                 #No of nodes
+        x   =   np.arange(0,n,1)                                    #value of x at every node
+        pe  =   peclet[iterator]               
+        delx=   0.1                                                 #space step
+        Texact=np.zeros(n)                                          
+        pec=pe*delx                                                 #cell peclet number
+        aa=0.5+pec/4                                                #coefficients in TDMA formulation
+        bb=0.5-pec/4                                                #coefficients in TDMA formulation
+        b=np.zeros(n-2)                                             #b vector in Ax=b formulation
+        b[n-3]=-1*bb                                            
+        A=np.zeros((n-2,n-2),dtype='f')                             #coefficient matrix
+        for i in range(n-2):
+            A[i][i]=-1
+            if i>0:
+                A[i][i-1]=aa
+            if i<n-3:
+                A[i][i+1]=bb
+        T=TDMA(A,b)                                                 #numerical solution
+        T=np.insert(T,0,0)                                          #Adding boundary condition
+        T=np.append(T,1)                                            #Adding boundary condition
+        axs[iterator].plot(x,T,label='fds')
+        try:
+            for i in range(n):
+                Texact[i]=(math.exp(pe*x[i])-1)/(math.exp(pe)-1)
+            for i in range(n):
+                Texact[i]=Texact[i]/Texact[n-1]
+            axs[iterator].plot(x,Texact,label='exact')
+            plt.text(0.98,0.95, "Pe"+pe, horizontalalignment='right', verticalalignment='top', transform=ax.transAxes)
+        except:
+            plt.xlim(80,99)
+            print("Peclet number two high for computer to calculate. ","Peclet number being ",pe)
+        axs[iterator].legend()
+    plt.show()
+peclet=np.array([0.01,0.4,2,10,30,50,70])
+visulaise(peclet)
